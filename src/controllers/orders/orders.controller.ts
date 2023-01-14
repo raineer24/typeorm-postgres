3db4c9c62290138897a3f63caf356ccb73cb0327
raineer <delaritaraineer81@gmail.com>
2023-01-14 21:36:06 +0800

    export const Orders

--- src/controllers/orders/orders.controller.ts
+++ src/controllers/orders/orders.controller.ts
@@ -1,3 +1,21 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
 
-export const Orders = async (req: Request, res: Response) => { }
+import { Order } from '../../orm/entities/orders/Orders';
+export const Orders = async (req: Request, res: Response) => {
+  const orders = await getRepository(Order).find({
+    where: { complete: true },
+    relations: ['order_items'],
+  });
+
+  res.send(
+    orders.map((order: Order) => ({
+      id: order.id,
+      name: order.name,
+      email: order.email,
+      total: order.total,
+      created_at: order.created_at,
+      order_items: order.order_items,
+    })),
+  );
+};
