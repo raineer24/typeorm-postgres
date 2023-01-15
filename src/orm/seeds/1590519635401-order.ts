24417888ea262ec44113f9f79908aa453bc76b15
raineer <delaritaraineer81@gmail.com>
2023-01-15 14:58:11 +0800

    orderitemrepo

--- src/orm/seeds/1590519635401-order.ts
+++ src/orm/seeds/1590519635401-order.ts
@@ -3,24 +3,38 @@ import { randomInt } from 'crypto';
 import * as faker from 'faker';
 import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
 
-import { Customer } from '../../orm/entities/customer/Customer';
-import { Link } from '../../orm/entities/link/Link';
 import { Order } from '../../orm/entities/orders/Orders';
 import { OrderItem } from '../entities/order-item/Order-item';
 
 export class SeedCustomers1590519635405 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    const linkRepository = getRepository(Link);
+    const orderRepository = getRepository(Order);
+    const orderItemRepository = getRepository(OrderItem);
 
     for (let i = 0; i < 30; i++) {
-      const customer = new Customer();
-      customer.id = i + 1;
-      await linkRepository.save({
+      const order = await orderRepository.save({
+        user_id: randomInt(2, 31),
         code: faker.random.alphaNumeric(6),
-        customer,
-        price: faker.commerce.price(),
+        ambassador_email: faker.internet.email(),
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName();
+        email: faker.internet.email(),
+        complete: true
       });
+
+      for (let j = 0; j < randomInt(1, 5); j++) {
+        await orderItemRepository.save({
+          order,
+          product_title: faker.lorem.words(2),
+          price: randomInt(2, 31),
+          quantity: randomInt(2, 31),
+          admin_revenue: randomInt(2, 31),
+          ambassador_revenue: randomInt(2, 31),
+        });
+      }
     }
+
+    
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
